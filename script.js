cf71b73889a3e9b67cc5d9f0a22587a3e1515c77
RafaelLucasP <rafael.lucas_2015@hotmail.com>
2021-03-29 16:14:15 -0300

    Atualização com fatorial

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+//Variavel numbers recebe os elementos da DOM com classe number
 var numbers = document.getElementsByClassName("number")
+//Variavel operator recebe os elementos da DOM com classe operator
 var operators = document.getElementsByClassName("operator")
 
-
+//Função que retorna o valor atual do display
 function getOutput() {
     return document.getElementById("output-value").innerText
 }
 
+//Função que escreve o valor digitado ou calculado no display, sendo que o valor máximo de digitos é 18
 function printOutput(num){
     if(num == "" ){
         document.getElementById("output-value").innerText=num
@@ -14,62 +17,115 @@ function printOutput(num){
     }
 }
 
+//Função que retorna o valor atual do histórico do display
 function getHistory() {
     return document.getElementById("history-value").innerText
 }
 
+//Função que escreve um valor no histórico do display
 function printHistory(num){
     document.getElementById("history-value").innerText=num
 }
 
+// Função para limpar o display e o histórico
+function clearAll(){
+    printHistory("")
+    printOutput("")    
+}
 
-
-
-
+/* 
+ * Cria um eventListener para cada botão numérico do teclado
+ * Desta forma quando um número for clicado será disparado um evento
+ * Com o evento disparado o valor é escrito na tela comprintOutput
+ */
 for (const number of numbers) {
     number.addEventListener("click", ()=>{
         printOutput(number.id)
     })
 }
 
+/* 
+ * Cria um eventListener para cada botão com operando do teclado
+ * Desta forma quando um número for clicado será disparado um evento
+ * Com o evento disparado realizará um tarefa
+ */
 for (const operator of operators) {
     operator.addEventListener("click", ()=>{
         switch (operator.id) {
+
+            /* 
+             * Caso a entrada seja CLEAR
+             * Chama as funções para escrever na tela com uma String vazia
+             * O resultado é limpar o display e o histórico
+             */
             case 'clear':
-                printHistory("")
-                printOutput("")
+                clearAll()
             break;
     
+            /*
+            * Caso a entrada seja BACKSPACE
+            * Cria uma variavel tempBackspace que recebe a String do display sem o ultimo caracter da esquerda para direita
+            * Em seguida limpa a tela e escreve o valor de tempBackspace           
+            */
             case 'backspace':
                 var tempBackspace = getOutput().substring(0,getOutput().length-1)
                 printOutput("")
                 printOutput(tempBackspace)
             break;
-            
-            case '%':
-                var result = getOutput()/100
-                printOutput("")
-                printOutput(result)
-                break;
 
-            case 'equal':
-                var history = getHistory()
-                var output = getOutput()
-                if (history != '' && output != '') {   
-                    printHistory("")
+            /*
+            * Caso seja !
+            * Verifica se a entrada é maior que 20
+            * Se sim o número seria demasiadamente grande, portanto é representado com infinito na display
+            * Caso contrário, calcula o fatorial e retorna seu valor no display
+            */
+            case '!':
+                if(getOutput() > 20){
                     printOutput("")
-                    printOutput(eval(`${history} ${output}`))  
+                    printOutput("Infinity")
                 }
-                
-                
+                else{
+                    let fatResult = 1
+                    for (let i = 1; i <= getOutput() ; i++) {
+                        fatResult *= i
+                    }
+                    printOutput("")
+                    printOutput(fatResult)
+                }   
             break;
 
-            default:
+            /*
+             * Caso a entrada seja = 
+             * Se existe um histórico e um valor no display 
+             * Então limpa a tela e o display e printa no display o valor da operação
+             * Caso contrário não realiza a operação
+             */
+            case 'equal':
+                var result  = eval(`${getHistory()} ${getOutput()}`)
+                if (getHistory() != '' && getOutput() != '') {   
+                    clearAll()
+                    printOutput(result)  
+                }
+            break;
 
+            /**
+             * Caso seja qualquer outro operador
+             * Se o display não estiver vazio
+             * Então verifica se o historico não é vazio
+             * Se Sim então calcula o resultado da operação e escreve no historico e a operção pressionada
+             * Se não escreve no histórico o valor do display e a operação pressionada
+             */
+            default:
                 if (getOutput() != '' ) {
-                    printHistory(`${getOutput()} ${operator.id}`)
-                    printOutput("")  
-                }    
+                    if(getHistory() != ''){
+                        var result  = eval(`${getHistory()} ${getOutput()}`)
+                        clearAll()
+                        printHistory(`${result} ${operator.id}`)  
+                    }else {
+                        printHistory(`${getOutput()} ${operator.id}`)
+                        printOutput("")  
+                    }
+                }
             break;
         }
     })
